be0b22e9f307f2e6e32fae85ddde2425bb787c69
FarisDani <farisdani84@gmail.com>
2024-06-23 16:15:07 +0700

    membuat delete pet, membuat update pet, dan menambahkan beberapa eror handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.text.google.fonts)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
